5a39e25b3acb5d356de9acba0797b96e80607acc
Basil Vetas <basilvetas@gmail.com>
2017-12-09 22:40:20 -0500

    initial commit

--- __main__.py
+++ __main__.py
@@ -1,15 +1,6 @@
 import tornado.ioloop
 import tornado.web
 
-# (1) If you would like extra credit on the mid-term, you can earn back X points by creating a 
-# containerized tornado REST API that accepts a query for a GET request like ?user=username and 
-# returns "Hello, <username>" as the response. I'll evaluate on a pass/fail basis by cd'ing into 
-# the container directory, building the container, running the container, and making an API 
-# request to it. You can submit your work by emailing me a github link (and giving me access, if 
-# it's private) with the subject line "MLP Extra Credit" sometime before the final. I'll follow 
-# the procedure outlined in class to build and run your container. The percentage points you can 
-# earn back will be X = 20 * ((100. - G) / G), where G is the grade you earned on the mid-term. 
-
 ## REST API that accepts a query for a GET request like ?user=username
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
